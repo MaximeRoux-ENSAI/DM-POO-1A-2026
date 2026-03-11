82ad9ded943363e2374ea4511e7425bf0a6a8aa6
MaximeRoux-ENSAI <maxime.roux@eleve.ensai.fr>
2026-03-11 23:48:25 +0100

    ✨ feat(joueur): ajout de la classe Coach  héritant de _Personne

--- src/coach.py
+++ src/coach.py
@@ -1,1 +1,15 @@
 """Implémentation de la classe Coach."""
+
+from .personne import _Personne
+
+
+class Coach(_Personne):
+    """Modéliser un coach. (Hérite de la classe _Personne)
+
+    Parameters
+    ----------
+    pseudo : str
+        Le pseudonyme du coach.
+    """
+
+    pass
