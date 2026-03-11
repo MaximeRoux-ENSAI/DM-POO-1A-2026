5e3afbd4bac4d4ac64c551d6cd609b54e294544b
MaximeRoux-ENSAI <maxime.roux@eleve.ensai.fr>
2026-03-11 23:48:04 +0100

    ✨ feat(joueur): ajout de la classe Joueur héritant de _Personne

--- src/joueur.py
+++ src/joueur.py
@@ -1,1 +1,15 @@
 """Implémentation de la classe Joueur."""
+
+from src.personne import _Personne
+
+
+class Joueur(_Personne):
+    """Modéliser un joueur. (Hérite de la classe _Personne)
+
+    Parameters
+    ----------
+    pseudo : str
+        Le pseudonyme du joueur.
+    """
+
+    pass
