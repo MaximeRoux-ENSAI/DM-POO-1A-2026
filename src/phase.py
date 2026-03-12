221e35792491a60e32f690af8e245a70b9401193
MaximeRoux-ENSAI <maxime.roux@eleve.ensai.fr>
2026-03-12 10:23:59 +0100

    ✨ feat(phase): implémentation de la classe abstraite Phase

--- src/phase.py
+++ src/phase.py
@@ -1,1 +1,31 @@
-"""Implémentation de la classe Phase."""
+"""Implémentation de la classe abstraite Phase."""
+
+from abc import abstractmethod
+from copy import deepcopy
+
+from src.competition import Competition
+
+
+class Phase(Competition):
+    """Modéliser une phase d'un tournoi."""
+
+    TABLEAU_VIDE = None
+
+    def __init__(self) -> None:
+        self._tableau = deepcopy(self.TABLEAU_VIDE)
+
+    @abstractmethod
+    def simuler_tirage(self) -> None:
+        """Simuler le tirage de la phase."""
+        ... # Ou pass à la place de ..., les deux sont possibles.
+
+
+    @abstractmethod
+    def simuler_tours(self) -> None:
+        """Simuler tous les tours de la phase."""
+        ... # Ou pass à la place de ..., les deux sont possibles.
+
+    def simuler(self) -> None:
+        """Simuler toute la phase."""
+        self.simuler_tirage()
+        self.simuler_tours()
